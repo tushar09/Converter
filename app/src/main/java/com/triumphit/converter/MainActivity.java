f02c39b2d61c3acb0085d409397d5ffe0121cedd
tushar_shah <wtushar09@gmail.com>
2015-09-05 15:11:20 +0600

    Admob added, error fixed, and indeterminate progressbar changed and utilized

--- app/src/main/java/com/triumphit/converter/MainActivity.java
+++ app/src/main/java/com/triumphit/converter/MainActivity.java
@@ -1,16 +1,21 @@
 package com.triumphit.converter;
 
 import android.annotation.TargetApi;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -21,10 +26,15 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 import com.andexert.library.RippleView;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,6 +53,8 @@ public class MainActivity extends AppCompatActivity implements CustomEvents {
     SharedPreferences sp;
     CustomEvents ce;
     TextView tvRate, tvDate, tvTime, tvAsk, tvBid;
+    ProgressBar pb;
+    private AdView mAdView;
 
     //Abir
     private String url1 = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20%28%22USDEUR%22%29&env=store://datatables.org/alltableswithkeys";
@@ -61,6 +73,10 @@ public class MainActivity extends AppCompatActivity implements CustomEvents {
         setContentView(R.layout.front_final);
         //getS/upportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#d1006c")));
         ce = this;
+
+        mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
 
         ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
         if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
@@ -86,6 +102,7 @@ public class MainActivity extends AppCompatActivity implements CustomEvents {
         tvBid = (TextView) findViewById(R.id.tvBid);
         edfrom = (EditText) findViewById(R.id.edfrom);
         edto = (EditText) findViewById(R.id.edto);
+        pb = (ProgressBar) findViewById(R.id.progressBar2);
 
 
 
@@ -103,6 +120,7 @@ public class MainActivity extends AppCompatActivity implements CustomEvents {
 
             }
         });
+
 
 
         RippleView fromRipple = (RippleView) findViewById(R.id.fromRipple);
@@ -137,7 +155,9 @@ public class MainActivity extends AppCompatActivity implements CustomEvents {
             public void onClick(View v) {
                 String g = "" + tvFrom.getText() + tvTo.getText() + "rate";
                 String name = tvFrom.getText().toString() + tvTo.getText().toString();
+
                 if(connected){
+                    pb.setVisibility(View.VISIBLE);
                     String url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20%28%22" + tvFrom.getText() + tvTo.getText() + "%22%29&env=store://datatables.org/alltableswithkeys";
                     obj = new HandleXML(url, MainActivity.this, ("" + tvFrom.getText().toString() + tvTo.getText().toString()), ce);
                     obj.fetchXML();
@@ -161,12 +181,68 @@ public class MainActivity extends AppCompatActivity implements CustomEvents {
 
             }
         });
-        //c=obj.getcurrency();
-        /*for(int i = 0; i < c.size(); i++) {
-            Log.d(c.get(i).getName(),"ABir");
-        }*/
-
         showComponent();
+        //rate us count
+        try {
+
+            // Get the app's shared preferences
+            SharedPreferences app_preferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+            // Get the value for the run counter
+            int counter = app_preferences.getInt("counter", 0);
+
+            // Do every x times
+            int RunEvery = 3;
+
+            if(counter != 0  && counter % RunEvery == 0 )
+            {
+                //Toast.makeText(this, "This app has been started " + counter + " times.", Toast.LENGTH_SHORT).show();
+
+                AlertDialog.Builder alert = new AlertDialog.Builder(
+                        MainActivity.this);
+                alert.setTitle("Please rate");
+                alert.setIcon(R.drawable.ic_launcher); //app icon here
+                alert.setMessage("Thanks for using this free app. Please take a moment to rate it.");
+
+                alert.setPositiveButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,
+                                                int whichButton) {
+                                //Do nothing
+                            }
+                        });
+
+                alert.setNegativeButton("Rate it",
+                        new DialogInterface.OnClickListener() {
+
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                final String appName = getApplicationContext().getPackageName();
+                                try {
+                                    startActivity(new Intent(Intent.ACTION_VIEW,
+                                            Uri.parse("market://details?id="
+                                                    + appName)));
+                                } catch (ActivityNotFoundException anfe) {
+                                    startActivity(new Intent(
+                                            Intent.ACTION_VIEW,
+                                            Uri.parse("http://play.google.com/store/apps/details?id="
+                                                    + appName)));
+                                }
+
+                            }
+                        });
+                alert.show();
+            }
+
+
+            // Increment the counter
+            SharedPreferences.Editor editor = app_preferences.edit();
+            editor.putInt("counter", ++counter);
+            editor.commit(); // Very important
+
+        } catch (Exception e) {
+            //Do nothing, don't run but don't break
+        }
 
 
     }
@@ -286,13 +362,33 @@ public class MainActivity extends AppCompatActivity implements CustomEvents {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        //
+        switch (id){
+            case R.id.credit:
+                Intent i=new Intent(this,Credit.class);
+                startActivity(i);
+                return true;
+            case R.id.rate:
+                Uri uri = Uri.parse("market://details?id=" + this.getPackageName());
+                Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+                try {
+                    startActivity(goToMarket);
+                } catch (ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("http://play.google.com/store/apps/details?id=" + this.getPackageName())));
+                }
+                return true;
+            case R.id.contact:
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://www.facebook.com/triumphITbd"));
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
 
@@ -323,17 +419,38 @@ public class MainActivity extends AppCompatActivity implements CustomEvents {
         public void handleMessage(Message msg) {
 
             Bundle b = msg.getData();
+            double from;
+            try{
+                from = Double.parseDouble(edfrom.getText().toString());
+            }catch (NumberFormatException ne){
+                from = 1;
+            }
 
-            double from = Double.parseDouble(edfrom.getText().toString());
             //double to = Double.parseDouble(edfrom.getText().toString());
             double rate2 = Double.parseDouble(b.getString("rate"));
             edto.setText("" + (from * rate2));
+            //copy clip board
+            setClipboard(MainActivity.this,String.valueOf((from * rate2)));
+            Toast.makeText(getApplicationContext(), "Text Copied to Clipboard",
+                    Toast.LENGTH_SHORT).show();
 
             tvRate.setText("Rate: " + b.getString("rate"));
             tvDate.setText("Date: " + b.getString("date"));
             tvTime.setText("Time: " + b.getString("time"));
             tvAsk.setText("Ask: " + b.getString("ask"));
             tvBid.setText("Bid: " + b.getString("bid"));
+            pb.setVisibility(View.GONE);
         }
     };
+    //copy to click
+    private void setClipboard(Context context,String text) {
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
+            android.text.ClipboardManager clipboard = (android.text.ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+            clipboard.setText(text);
+        } else {
+            android.content.ClipboardManager clipboard = (android.content.ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+            android.content.ClipData clip = android.content.ClipData.newPlainText("Copied Text", text);
+            clipboard.setPrimaryClip(clip);
+        }
+    }
 }
